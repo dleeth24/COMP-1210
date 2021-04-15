bd73443ad39dcc614e2547dd2957cc785d9badf7
dleeth24 <82239098+dleeth24@users.noreply.github.com>
2021-04-15 15:09:45 -0500

    Add files via upload

---
+++ Project 1/JLetter.java
@@ -0,0 +1,28 @@
+/**
+* This program prints a large 'J' using the word
+* 'java' as the building blocks.
+*
+* Project 1
+* @author Davis Leeth - COMP 1213
+* @version 1/18/21
+*/
+public class JLetter 
+{
+/**
+* Prints 'J'.
+* @param args Command line arguments – not used.
+*/
+   public static void main(String[]args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA  ");
+      System.out.println("      JAVA  ");
+      System.out.println("      JAVA  ");
+      System.out.println("      JAVA  ");
+      System.out.println("J     JAVA  ");
+      System.out.println("JA    JAVA  ");
+      System.out.println(" JAVAJAVA   ");
+      System.out.println("  JAVAJA    ");
+   }
+}
---
+++ Project 1/MyLifeGoals.java
@@ -0,0 +1,29 @@
+/**
+* Simple program to print my short term, 
+* medium term, and long term life goals.
+*
+* Project 1
+* @author Davis Leeth - COMP 1213
+* @version 1/18/21
+*/
+public class MyLifeGoals 
+{
+/**
+* Prints life goals.
+* @param args Command line arguments – not used.
+*/
+   public static void main(String[]args)
+   {
+      System.out.println("Davis Leeth");
+      System.out.println();
+      System.out.println("My short-term life goals include performing well in" 
+         + " college, getting a computer science degree,"
+         + " and enjoying my time at Auburn as a student.");
+      System.out.println("My medium-term life goals are to get a job I enjoy" 
+         + " at a computer software firm, move to a different state,"
+         + " and start a family after buying a nice house.");
+      System.out.println("My long-term life goals are to retire early after a" 
+         + " successful career in the computer science industry and"
+         + " travel the world with my family.");   
+   }
+}
